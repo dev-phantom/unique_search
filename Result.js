ed41fd5304f0bf3c95c4486fb729c6f16f5e43d6
Elimihele God's favour <72051159+favourite1975@users.noreply.github.com>
2021-03-11 00:24:59 +0100

    Add files via upload

---
+++ Result.js
@@ -0,0 +1,11 @@
+/*function mybu() {
+   var element = document.style.body.color="black"
+   element.classList.toggle("dark-mode");
+}*/
+function mybu() {
+  var pElement = document.body;
+      
+      // let's select the <p> element's parent and s
+      pElement.parentElement.style.background = "gold";
+
+}
